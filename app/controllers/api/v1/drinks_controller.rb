d60987fa4bd0936fae21c46dd5fe5279d8fc3ee5
Jonathan Atkins <jonathanjatkins89@gmail.com>
2026-06-05 18:45:36 -0700

    Implement create action for DrinksController and update routes; add request specs for drink creation

--- app/controllers/api/v1/drinks_controller.rb
+++ app/controllers/api/v1/drinks_controller.rb
@@ -3,4 +3,22 @@ class Api::V1::DrinksController < ApplicationController
     drinks = Drink.all
     render json: drinks, status: :ok
   end
+
+  def create
+    drink = Drink.create!(drink_params)
+
+    if drink
+      drink.save
+      render json: drink, status: :created
+    else
+      render json: { error: "Unable to create drink" }, status: :unprocessable_entity
+    end
+  end
+
+ private
+
+  def drink_params
+    params.permit(:name, :category, :alcoholic)
+  end
+  
 end
--- spec/requests/api/v1/drinks_spec.rb
+++ spec/requests/api/v1/drinks_spec.rb
@@ -1,11 +1,12 @@
 require "rails_helper"
 
 RSpec.describe "Drinks App", type: :request do
-  before(:each) do
-    @mojito       = Drink.create!(name: "Mojito", category: "Rum", alcoholic: true)
-    @old_fasioned = Drink.create!(name: "Old Fashioned", category: "Whiskey", alcoholic: true)
-  end
   describe "GET /api/v1/drinks" do
+    before(:each) do
+      @mojito = Drink.create!(name: "Mojito", category: "Rum", alcoholic: true)
+      @old_fashioned = Drink.create!(name: "Old Fashioned", category: "Whiskey", alcoholic: true)
+    end
+
     describe "happy path" do
       it "returns a 200 status code" do
         get "/api/v1/drinks"
@@ -19,25 +20,35 @@ RSpec.describe "Drinks App", type: :request do
         drinks = JSON.parse(response.body)
 
         expect(drinks.count).to eq(2)
-        
+
         expect(drinks.first["name"]).to eq("Mojito")
         expect(drinks.first["category"]).to eq("Rum")
         expect(drinks.first["alcoholic"]).to eq(true)
-        
+
         expect(drinks.second["name"]).to eq("Old Fashioned")
         expect(drinks.second["category"]).to eq("Whiskey")
         expect(drinks.second["alcoholic"]).to eq(true)
       end
     end
+  end
 
-    # describe "sad path" do
-    #   it "returns an empty array when there are no drinks" do
-    #     get "/api/v1/drinks"
+  describe "POST /api/v1/drinks" do
+    describe "happy path" do
+      it "can add a drink to the drink menu" do
+        post "/api/v1/drinks", params: {
+          name: "Margarita",
+          category: "Tequila",
+          alcoholic: true
+        }
 
-    #     drinks = JSON.parse(response.body)
+        drink = JSON.parse(response.body)
 
-    #     expect(response).to have_http_status(:ok)
-    #     expect(drinks).to eq([])
-    #   end
+        expect(response).to have_http_status(:created)
+        expect(Drink.all.count).to eq(1)
+        expect(drink["name"]).to eq("Margarita")
+        expect(drink["category"]).to eq("Tequila")
+        expect(drink["alcoholic"]).to eq(true)
+      end
+    end
   end
 end
