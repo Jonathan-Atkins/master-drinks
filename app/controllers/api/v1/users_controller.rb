70db5e3d4e3cdb677227e5cd404efef0a322b72f
Jonathan Atkins <jonathanjatkins89@gmail.com>
2026-06-23 22:01:02 -0700

    update: completed user_controller, all test passing

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :set_user, only: [ :show ]
+  before_action :set_user, only: [:show, :update, :destroy]
 
   def index
     users = User.all
@@ -12,20 +12,43 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
+
     if user.save
       render json: user, status: :created
     else
-      render json: ErrorSerializer.format(user), status: :unprocessable_content
+      render json: ErrorSerializer.format(user),
+             status: :unprocessable_content
     end
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: @user, status: :ok
+    else
+      render json: ErrorSerializer.format(@user),
+             status: :unprocessable_content
+    end
+  end
+
+  def destroy
+    @user.destroy
+
+    head :no_content
   end
 
   private
 
-    def user_params
-      params.permit(:name, :username, :email, :password, :password_confirmation)
-    end
+  def user_params
+    params.permit(
+      :name,
+      :username,
+      :email,
+      :password,
+      :password_confirmation
+    )
+  end
 
-    def set_user
-      @user = User.find(params[:id])
-    end
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
--- spec/requests/api/v1/user_spec.rb
+++ spec/requests/api/v1/user_spec.rb
@@ -69,8 +69,41 @@ RSpec.describe "User App", type: :request do
         expect(result).not_to have_key("password")
         expect(result).not_to have_key("password_confirmation")
 
-        # Add this after creating the user serializer:
+        # Add after creating the user serializer:
         # expect(result).not_to have_key("password_digest")
+      end
+    end
+
+    describe "PATCH /api/v1/users/:id" do
+      it "can update a user" do
+        user = User.create!(@user_params)
+        updated_params = {
+          name: "Alice Smith",
+          username: "AliceInWonderLand"
+        }
+
+        patch "/api/v1/users/#{user.id}", params: updated_params
+
+        result = JSON.parse(response.body)
+        user.reload
+
+        expect(response).to have_http_status(:ok)
+        expect(result["name"]).to eq("Alice Smith")
+        expect(user.name).to eq("Alice Smith")
+        expect(user.username).to eq("AliceInWonderLand")
+      end
+    end
+
+    describe "DELETE /api/v1/users/:id" do
+      it "can delete a user" do
+        user = User.create!(@user_params)
+
+        expect do
+          delete "/api/v1/users/#{user.id}"
+        end.to change(User, :count).by(-1)
+
+        expect(response).to have_http_status(:no_content)
+        expect(response.body).to be_empty
       end
     end
   end
@@ -119,6 +152,44 @@ RSpec.describe "User App", type: :request do
         )
       end
     end
+
+    describe "PATCH /api/v1/users/:id" do
+      it "returns an error if the user cannot be found" do
+        patch "/api/v1/users/999", params: { name: "Updated Name" }
+
+        result = JSON.parse(response.body)
+
+        expect(response).to have_http_status(:not_found)
+        expect(result["errors"]).to include(
+          "Couldn't find User with 'id'=\"999\""
+        )
+      end
+
+      it "returns an error if the updated user is invalid" do
+        user = User.create!(@user_params)
+
+        patch "/api/v1/users/#{user.id}", params: { name: nil }
+
+        result = JSON.parse(response.body)
+        user.reload
+
+        expect(response).to have_http_status(:unprocessable_content)
+        expect(result["errors"]).to include("Name can't be blank")
+        expect(user.name).to eq("Alice")
+      end
+    end
+
+    describe "DELETE /api/v1/users/:id" do
+      it "returns an error if the user cannot be found" do
+        delete "/api/v1/users/999"
+
+        result = JSON.parse(response.body)
+
+        expect(response).to have_http_status(:not_found)
+        expect(result["errors"]).to include(
+          "Couldn't find User with 'id'=\"999\""
+        )
+      end
+    end
   end
 end
-
