e306f38b4e4a7cc53fc4c4cae105a29fe52bf617
Jonathan Atkins <jonathanjatkins89@gmail.com>
2026-08-20 16:38:00 -0700

    refactor: format origins in CORS configuration for improved readability

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173",
-      "https://master-drinks-frontend.vercel.app,
-      https://barbuddy.itsjonathanatkins.com"
+    origins(
+      "http://localhost:5173",
+      "https://master-drinks-frontend.vercel.app",
+      "https://barbuddy.itsjonathanatkins.com"
+    )
 
     resource "*",
       headers: :any,
