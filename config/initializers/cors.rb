ce0e2347a3f9751de4a0bd36d0195d2363f00ba8
Jonathan Atkins <jonathanjatkins89@gmail.com>
2026-08-20 16:24:31 -0700

    feat: update CORS configuration to include additional allowed origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:5173",
-      "https://master-drinks-frontend.vercel.app"
+      "https://master-drinks-frontend.vercel.app,
+      https://barbuddy.itsjonathanatkins.com"
 
     resource "*",
       headers: :any,
